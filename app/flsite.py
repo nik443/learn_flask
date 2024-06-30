96934069cba7b3ed98efce63c51e625956fa4833
nik443 <nikhram301@gmail.com>
2024-06-30 21:00:36 +1000

    add

--- app/flsite.py
+++ app/flsite.py
@@ -1,5 +1,4 @@
-from flask import Flask, render_template, url_for, request, flash
-
+from flask import Flask, render_template, url_for, request, flash, session, redirect, abort
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "nikita_master"
@@ -21,8 +20,10 @@ def about_page():
     return render_template("about.html", menu=menu, title="About")
 
 
-@app.route("/profile/<int:username>")
-def user_page(username):
+@app.route("/profile/<username>")
+def profile(username):
+    if "userLogged" not in session or session["userLogged"] != username:
+        abort(401)  # прерывание запроса с ошибкой 401 - нет доступа
     return f"Профиль пользователя {username}"
 
 
@@ -40,13 +41,19 @@ def contact():
 
 @app.errorhandler(404)
 def pageNotFound(error):
-    return render_template(template_name_or_list="page404.html", title="Страница не найдена", menu=menu), 404
+    return render_template(template_name_or_list="page404.html", title="Страница не найдена", menu=menu)
 
 
-# with app.test_request_context():
-#     print(url_for("index"))
-#     print(url_for("about_page"))
-#     print(url_for("user_page", username="11"))
+@app.route(rule="/login", methods=["POST", "GET"])
+def login():
+    if "userLogged" in session:
+        return redirect(url_for("profile", username=session["userLogged"]))
+    elif request.form["username"] == "root" and request.form["password"] == "1111":
+        session["userLogged"] = request.form["username"]
+        return redirect(url_for("profile", username=session["userLogged"]))
+
+    return render_template(template_name_or_list="login.html", title="Авторизация", menu=menu)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
